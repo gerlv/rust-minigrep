cd217ba13e334860fd20aea8aaf58d0a614e7b32
Oleg Gera <hi@oleggera.com>
2023-03-01 16:03:28 +0000

    refactor: config struct

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 use std::env;
 use std::fs;
-
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
-
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Query: {query}");
-    println!("File path: {file_path}");
+    println!("Query: {0}", config.query);
+    println!("File path: {0}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Poem to exist");
     
     println!("File contents:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
